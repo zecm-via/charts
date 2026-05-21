3cbd371b38fb1edc81f47dd43cbc88d3451382c1
Sebastian Schneider <s.schneider@noeol.de>
2026-05-21 14:17:00 +0200

    [TASK] Replace obsolete getFileFieldTCAConfig() call

--- Configuration/TCA/tx_charts_domain_model_chartdata.php
+++ Configuration/TCA/tx_charts_domain_model_chartdata.php
@@ -192,22 +192,21 @@ return (static function (string $extKey = 'charts') {
             'spreadsheet_assets' => [
                 'exclude' => true,
                 'label' => $ll . '.spreadsheet_assets',
-                'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
-                    'assets',
-                    [
-                        'appearance' => [
-                            'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/Database.xlf:tt_content.asset_references.addFileReference',
-                        ],
-                        'overrideChildTca' => [
-                            'types' => [
-                                '0' => [
-                                    'showitem' => '--palette--;;filePalette',
-                                ],
+                'config' => [
+                    'type' => 'file',
+                    'appearance' => [
+                        'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/Database.xlf:tt_content.asset_references.addFileReference',
+                    ],
+                    'overrideChildTca' => [
+                        'types' => [
+                            \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+                                'showitem' => '--palette--;;filePalette',
                             ],
                         ],
                     ],
-                    'xls,xlsx,ods'
-                ),
+                    'allowed' => 'xls,xlsx,ods',
+                    'maxitems' => 1,
+                ],
             ],
             'background_colors' => [
                 'exclude' => true,
